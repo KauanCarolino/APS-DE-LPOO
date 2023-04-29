9a0fa83ca3860403f96487c5ed036a6c0929f959
Kauan Carolino <kauancarolino248@gmail.com>
2023-04-29 15:30:26 -0300

    APS

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,5 +2,6 @@ public class Cliente {
     private String nome;
     private String enderecos[] = new String[3];
     private String cpf;
+    private String cnpj;
     private double limiteDeCredito;
 }
